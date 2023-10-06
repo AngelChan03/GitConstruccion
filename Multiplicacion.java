6bb18215021862dd7da66ef78f76f72d7555551f
DioneMartin <dione.gpe0102@gmail.com>
2023-10-05 18:05:37 -0600

    Actualizacion de la funcion

--- Multiplicacion.java
+++ Multiplicacion.java
@@ -2,20 +2,21 @@ public class Multiplicacion {
     private double numero1;
     private double numero2;
 
-    public double calcularMultiplicion(double numUno, double numDos){
+    Multiplicacion(){}
+    
+    public double calcularMultiplicacion(double numUno, double numDos){
         this.numero1 = numUno;
         this.numero2 = numDos;
-
         double resultado = 0;
-        for(i=0; i<numero2; i++){
+        for(int i=0; i<numero2; i++){
             resultado += numero1;
         }
-
         return resultado;
     }
 
     public static void main(String[] args) {
-        Multiplicacion multiplicar = new Multiplicacion(2,7);
-        System.out.println(multiplicar);
+        Multiplicacion multiplicar = new Multiplicacion();
+        double resultadoFinal = multiplicar.calcularMultiplicacion(15, 7);
+        System.out.println(resultadoFinal);
     }
 }
