92353fe426f1d9e1a48425d7a01e983e35193b6e
Àngel <a18001340@alumnos.uady.mx>
2023-10-05 23:00:28 -0600

    Añadir la estructuta base a la clase CalculadoraBasica

--- calculadoraBasica.java
+++ calculadoraBasica.java
@@ -1,3 +1,30 @@
-public class calculadoraBasica {
+public class CalculadoraBasica {
+    public void CalculadoraBasica(){
+
+    }
+
+    public void calcularSuma(){
+
+    }
+
+    public void calcularResta(){
+        
+    }
+
+    public void calcularMultiplicacion(){
+
+    }
+
+    public void calcularDivision(){
+
+    }
+    
+    public void calcularPotencia(){
+
+    }
+
+    public void calcularModulo(){
+
+    }
 
 }
