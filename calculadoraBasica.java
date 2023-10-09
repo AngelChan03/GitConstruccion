5d520a791de24edb1f7db3bca1e174e226b6f784
Kevin Medina <kevinmedina2509Jr@gmail.com>
2023-10-09 08:59:49 -0600

    fix: reparación del funcionalidad Potencia

--- calculadoraBasica.java
+++ calculadoraBasica.java
@@ -19,24 +19,9 @@ public class CalculadoraBasica {
 
     }
     
-    public  double calcularPotencia(double base, int exponente) {
-        double resultadoPotencia = 0;
-        double nuevaBase = base;
-        int nuevoExponente = exponente;
-
-        if (nuevoExponente == 0) {
-            // Cualquier número elevado a la potencia 0 es 1
-            resultadoPotencia = 1.0;
-            return resultadoPotencia; 
-        } else if (nuevoExponente < 0) {
-            // Manejo de exponentes negativos: calculamos la inversa de la base elevada al exponente positivo
-            resultadoPotencia = 1.0 / (nuevaBase * calcularPotencia(nuevaBase, -nuevoExponente- 1));
-            return  resultadoPotencia;
-        } else {
-            // Caso general: multiplicamos la base por sí misma (exponente - 1) veces
-            resultadoPotencia = nuevaBase * calcularPotencia(nuevaBase, nuevoExponente - 1);
-            return resultadoPotencia;
-        }
+    public static double calcularPotencia(double base, int exponente) {
+        double resultadoExponente = Math.pow(base, exponente);
+        return resultadoExponente;
     }
 
     public void calcularModulo(){
