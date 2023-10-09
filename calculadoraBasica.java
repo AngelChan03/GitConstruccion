c6684ffbc014072fb8af6e3125cc20168cafa3e4
Kevin Medina <kevinmedina2509Jr@gmail.com>
2023-10-08 19:32:37 -0600

    feat: agregando método calcularPotencia

--- calculadoraBasica.java
+++ calculadoraBasica.java
@@ -19,8 +19,24 @@ public class CalculadoraBasica {
 
     }
     
-    public void calcularPotencia(){
+    public  double calcularPotencia(double base, int exponente) {
+        double resultadoPotencia = 0;
+        double nuevaBase = base;
+        int nuevoExponente = exponente;
 
+        if (nuevoExponente == 0) {
+            // Cualquier número elevado a la potencia 0 es 1
+            resultadoPotencia = 1.0;
+            return resultadoPotencia; 
+        } else if (nuevoExponente < 0) {
+            // Manejo de exponentes negativos: calculamos la inversa de la base elevada al exponente positivo
+            resultadoPotencia = 1.0 / (nuevaBase * calcularPotencia(nuevaBase, -nuevoExponente- 1));
+            return  resultadoPotencia;
+        } else {
+            // Caso general: multiplicamos la base por sí misma (exponente - 1) veces
+            resultadoPotencia = nuevaBase * calcularPotencia(nuevaBase, nuevoExponente - 1);
+            return resultadoPotencia;
+        }
     }
 
     public void calcularModulo(){
