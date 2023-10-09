67167cf986d561d569511a01c85a7adaaf6af876
Àngel <a18001340@alumnos.uady.mx>
2023-10-08 19:25:18 -0600

    Añadir la funcionalidad resta a la clase CalculadoraBasica

--- calculadoraBasica.java
+++ calculadoraBasica.java
@@ -1,3 +1,31 @@
-public class calculadoraBasica {
+public class CalculadoraBasica{
+    public void CalculadoraBasica(){
+
+    }
+
+    public void calcularSuma(){
+
+    }
+
+    public double calcularResta(double primerValorResta, double segundoValorResta){
+        double restaTotal = primerValorResta - segundoValorResta;
+        return restaTotal;
+    }
+
+    public void calcularMultiplicacion(){
+
+    }
+
+    public void calcularDivision(){
+
+    }
+    
+    public void calcularPotencia(){
+
+    }
+
+    public void calcularModulo(){
+
+    }
 
 }
